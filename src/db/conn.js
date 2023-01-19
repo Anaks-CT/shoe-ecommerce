e470c1a06c5b308feeebaad448b6b7427fb1e89a
Anaks-CT <anaksthayyil30@gmail.com>
2023-01-19 12:41:52 +0530

    checking error

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://127.0.0.1:27017/ecommerce_userdetails", {
+mongoose.connect("mongodb+srv://anaks:anaks@cluster0.1xcvuno.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     // useCreateIndex:true

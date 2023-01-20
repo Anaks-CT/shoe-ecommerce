3635437051b74b4f323e9b7ab24bb28c88927836
Anaks-CT <anaksthayyil30@gmail.com>
2023-01-20 14:42:05 +0530

    paypal fix

--- utility/paypal.js
+++ utility/paypal.js
@@ -1,65 +1,65 @@
-const paypal = require('paypal-rest-sdk');
-const Register = require('../src/models/database')
-const coupon = require('../src/models/coupon')
+const paypal = require("paypal-rest-sdk");
+const Register = require("../src/models/database");
+const coupon = require("../src/models/coupon");
 paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'AZpBGzCJHdNSn2MRqlhLctThU5uj3xIJ8ZCN4ae0XaeTIYWOM0IoV9GgREpTPDEu4xjHV2fkkvYR9s2S',
-    'client_secret': 'EF5GWkfwo2vNzPQYpHUf6IFPIPYIWQ80SYFe02B2AQ_jn8lro3iGLaZ7cb42bOV3s8PBiwHyUCkZzHmf'
-  });
+  mode: "sandbox", //sandbox or live
+  client_id:
+    "AXxgb385M9x9huQ92gVjkdAZlsJlfjuOaQimQQlIE68VTIFJBu6l7EZZPlrwcMV9BWjtlc3HUT6dlEBp",
+  client_secret:
+    "EE8XlipR2U1Omg1Z-Bm0OeZbMpFfFNb5OU4p-qM-1ZJezOBUCJysWGL6xw72lzRbaa9x1Tlzqv_wnzpq",
+});
 
-const paypalgate = async (req,res) =>{
-    const orderDetialsId = req.query.orderDetials
-    console.log(orderDetialsId);
-    const user = await Register.findOne({ Email: req.session.user });
-    const totalPrice = user.cart.totalPrice;
-    const discount = req.query.discount
-    let finalPrice = (totalPrice-discount)
+const paypalgate = async (req, res) => {
+  const orderDetialsId = req.query.orderDetials;
+  console.log(orderDetialsId);
+  const user = await Register.findOne({ Email: req.session.user });
+  const totalPrice = user.cart.totalPrice;
+  const discount = req.query.discount;
+  let finalPrice = totalPrice - discount;
 
-    const create_payment_json = {
-        "intent": "sale",
-        "payer": {
-          "payment_method": "paypal"
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: `https://runinstyle.co/checkout/placeOrder/${orderDetialsId}`,
+      cancel_url: "https://runinstyle.co/checkoutPage",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: `Order Number-${orderDetialsId}`,
+              sku: `Order Number-${orderDetialsId}`,
+              price: finalPrice,
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
         },
-        "redirect_urls": {
-      "return_url": "https://runinstyle.co/checkout/placeOrder/"+orderDetialsId,
-          "cancel_url": "https://runinstyle.co/checkoutPage"
+        amount: {
+          currency: "USD",
+          total: finalPrice,
         },
-        "transactions": [{
-          "item_list": {
-            "items": [{
-              "name": "item",
-              "sku": "item",
-              "price": finalPrice,
-              "currency": "USD",
-              "quantity": 1
-            }]
-          },
-          "amount": {
-            "currency": "USD",
-            "total": finalPrice
-          },
-          "description": "This is the payment description."
-        }]
-      };
-      paypal.payment.create(create_payment_json, async function (error, payment) {
-        if (error) {
-          throw error;
-        } else {
-          for (let i = 0; i < payment.links.length; i++) {
-            if (payment.links[i].rel === "approval_url") {
-              res.redirect(payment.links[i].href);
-  
-            }
+        description: "runinstyle.",
+      },
+    ],
+  };
+  paypal.payment.create(create_payment_json, async function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === "approval_url") {
+          res.redirect(payment.links[i].href);
         }
       }
-    });
-    
-  }
-
-
-
-
+    }
+  });
+};
 
 module.exports = {
-    paypalgate
-}
+  paypalgate,
+};

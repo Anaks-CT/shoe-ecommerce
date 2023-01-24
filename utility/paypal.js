1976192a7c02b112a52e2b2b7373000dca233c15
Anaks-CT <anaksthayyil30@gmail.com>
2023-01-24 20:32:22 +0530

    papayl attempt 11

--- utility/paypal.js
+++ utility/paypal.js
@@ -1,6 +1,6 @@
-const paypal = require("paypal-rest-sdk");
-const Register = require("../src/models/database");
-const coupon = require("../src/models/coupon");
+// const paypal = require("paypal-rest-sdk");
+// const Register = require("../src/models/database");
+// const coupon = require("../src/models/coupon");
 
 
 // module.exports = {
